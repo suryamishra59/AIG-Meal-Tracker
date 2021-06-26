3d599be8c0da3fc458204697068b0c0a7eeaced0
Surya Deep Mishra <suryamishra59@gmail.com>
2021-06-26 15:16:02 +0530

    Added CRUD Meal APIs

--- server/controllers/meal.js
+++ server/controllers/meal.js
@@ -3,10 +3,32 @@ const { Meal } = require('../models');
 const { authenticateJWT } = require('../auth');
 
 module.exports = (app) => {
-    app.get(process.env.MEAL_API, authenticateJWT, (req, res) => {
-        const meal = new Meal();
+    app.post(process.env.MEAL_API, authenticateJWT, (req, res) => {
+        const meal = new Meal({
+            date: req.body.date,
+            mealName: req.body.mealName,
+            calories: req.body.calories,
+        });
         meal.createMeal(req).then(resp => {
             res.send(getExpressResponse(res, resp));
         });
+    });
+
+    app.get(process.env.MEAL_API, authenticateJWT, (req, res) => {
+        const meal = new Meal();
+        const resp = meal.getMeal(req);
+        res.send(getExpressResponse(res, resp));
+    });
+
+    app.put(process.env.MEAL_BY_ID_API, authenticateJWT, (req, res) => {
+        const meal = new Meal();
+        const resp = meal.updateMeal(req, req.params.id, req.body);
+        res.send(getExpressResponse(res, resp));
+    });
+
+    app.delete(process.env.MEAL_BY_ID_API, authenticateJWT, (req, res) => {
+        const meal = new Meal();
+        const resp = meal.deleteMeal(req.params.id);
+        res.send(getExpressResponse(res, resp));
     });
 };
--- server/models/meal/meal.js
+++ server/models/meal/meal.js
@@ -1,15 +1,64 @@
-const { getResponseObject } = require('../../util');
+const { getResponseObject, writeFile } = require('../../util');
 const mealData = require('./meal.json');
+const { v4: uuid } = require('uuid');
 
 class Meal {
 
+    date;
+    mealName;
+    calories;
     constructor(object = {}) {
-
+        this.date = object.date;
+        this.mealName = object.mealName;
+        this.calories = object.calories;
     }
 
     async createMeal(req) {
         const user = req.user;
-        return getResponseObject();
+        mealData.push({
+            date: this.date,
+            mealName: this.mealName,
+            calories: this.calories,
+            uid: user.uid,
+            id: uuid()
+        });
+
+        writeFile('./models/meal/meal.json', JSON.stringify(mealData));
+        return getResponseObject(200, "Meal saved succesfully");
+    }
+
+    getMeal(req) {
+        const user = req.user;
+        const meals = require('./meal.json');
+        const filterMeals = meals.filter(meal => meal.uid === user.uid);
+
+        return getResponseObject(200, "Meals fetched succesfully", filterMeals);
+    }
+
+    updateMeal(req, id, payload) {
+        const meals = require('./meal.json');
+        const user = req.user;
+        for (let i = 0; i < meals.length; i++) {
+            const meal = meals[i];
+            if (meal.id === id && user.uid === meal.uid) {
+                meal.date = payload.date || meal.date;
+                meal.calories = payload.calories || meal.calories;
+                meal.mealName = payload.mealName || meal.mealName;
+
+                break;
+            }
+        }
+
+        writeFile('./models/meal/meal.json', JSON.stringify(mealData));
+        return getResponseObject(200, "Meal updated succesfully");
+    }
+
+    deleteMeal(id) {
+        const meals = require('./meal.json');
+        const filterMeals = meals.filter(meal => meal.id !== id);
+
+        writeFile('./models/meal/meal.json', JSON.stringify(filterMeals));
+        return getResponseObject(200, "Meal deleted succesfully", filterMeals);
     }
 }
 
